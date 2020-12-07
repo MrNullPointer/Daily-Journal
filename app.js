15d0b984e6f36b1f1521ec51d0b7ba5b0e1041a7
MrNullPointer <58353903+MrNullPointer@users.noreply.github.com>
2020-12-07 11:24:48 -0500

    Step - 12

--- app.js
+++ app.js
@@ -21,8 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render("home", { startingContent: homeStartingContent });
-  console.log(posts);
+  res.render("home", { startingContent: homeStartingContent, posts : posts });
 });
 
 app.get("/about", function (req, res) {
