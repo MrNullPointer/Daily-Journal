0de2b5c159acf47b5b19feffbcce1d2d4e668f8f
MrNullPointer <58353903+MrNullPointer@users.noreply.github.com>
2020-12-07 13:16:32 -0500

    Step - 18 Install loDash npm package

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const { forEach } = require("lodash");
+const _ = require("lodash");
+
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -26,12 +27,15 @@ app.get("/", function (req, res) {
 });
 
 app.get("/posts/:postName", function (req, res) {
-  const requestedTitle = req.params.postName;
+  const requestedTitle = _.lowerCase(req.params.postName);
   // Express routing : A new concept
-  console.log(requestedTitle); //anything typed on the path root/posts ....  will be console logged
+  // console.log(requestedTitle); //anything typed on the path root/posts ....  will be console logged
   posts.forEach(function (post) {
-    if (post.title === requestedTitle) {
+    const storedTitle = _.lowerCase(post.title);
+    if (storedTitle === requestedTitle) {
       console.log("match");
+    }else{
+      console.log("Not a match")
     }
   });
 });
