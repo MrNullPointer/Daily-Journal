9a055154872c546c653d1392767e66fb9b614461
MrNullPointer <58353903+MrNullPointer@users.noreply.github.com>
2020-12-07 12:54:50 -0500

    Step - 17

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const { forEach } = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -24,10 +25,15 @@ app.get("/", function (req, res) {
   res.render("home", { startingContent: homeStartingContent, posts: posts });
 });
 
-app.get("/posts/:test", function (req, res) {
+app.get("/posts/:postName", function (req, res) {
+  const requestedTitle = req.params.postName;
   // Express routing : A new concept
-  console.log(req.params.test); //anything typed on the path root/posts ....  will be console logged
-  res.redirect("/");
+  console.log(requestedTitle); //anything typed on the path root/posts ....  will be console logged
+  posts.forEach(function (post) {
+    if (post.title === requestedTitle) {
+      console.log("match");
+    }
+  });
 });
 
 app.get("/about", function (req, res) {
